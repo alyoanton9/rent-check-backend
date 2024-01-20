d75876cf9c142106a9a186805e448b35620e6ea0
Alyona Antonova <alyona.antonova@serokell.io>
2024-01-20 17:35:04 +0100

    cover 'CopyItemsFromFlatGroup' with tests, and refactor 'testGetGroupFlats' a bit

--- internal/repository/mixed_test.go
+++ internal/repository/mixed_test.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"github.com/samber/lo"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	e "rent-checklist-backend/internal/error"
@@ -10,11 +11,6 @@ import (
 
 func testMixedRepositories(t *testing.T, flatRepo FlatRepository, groupRepo GroupRepository,
 	itemRepo ItemRepository, userId uint64) {
-
-	testGetGroupFlats(t, flatRepo, groupRepo, userId)
-}
-
-func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRepository, userId uint64) {
 	flats := []model.Flat{
 		{
 			Address: "d",
@@ -24,10 +20,6 @@ func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRep
 			Address: "e",
 			OwnerId: userId,
 		},
-	}
-	for i := range flats {
-		err := flatRepo.CreateFlat(&(flats[i]))
-		require.NoError(t, err)
 	}
 
 	groups := []model.Group{
@@ -40,8 +32,21 @@ func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRep
 			UserId: userId,
 		},
 	}
-	for i := range groups {
-		err := groupRepo.CreateGroup(&(groups[i]))
+
+	testGetGroupFlats(t, flatRepo, groupRepo, &flats, &groups, userId)
+
+	testCopyItemsFromFlatGroup(t, itemRepo, &flats, &groups, userId)
+}
+
+func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRepository,
+	flats *[]model.Flat, groups *[]model.Group, userId uint64) {
+	for i := range *flats {
+		err := flatRepo.CreateFlat(&(*flats)[i])
+		require.NoError(t, err)
+	}
+
+	for i := range *groups {
+		err := groupRepo.CreateGroup(&(*groups)[i])
 		require.NoError(t, err)
 	}
 
@@ -54,26 +59,26 @@ func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRep
 	}{
 		{
 			name:          "valid 1",
-			groupId:       groups[0].Id,
-			flatId:        flats[0].Id,
-			expectedFlats: []model.Flat{flats[0]},
+			groupId:       (*groups)[0].Id,
+			flatId:        (*flats)[0].Id,
+			expectedFlats: []model.Flat{(*flats)[0]},
 		},
 		{
 			name:          "valid 2",
-			groupId:       groups[0].Id,
-			flatId:        flats[1].Id,
-			expectedFlats: []model.Flat{flats[1], flats[0]},
+			groupId:       (*groups)[0].Id,
+			flatId:        (*flats)[1].Id,
+			expectedFlats: []model.Flat{(*flats)[1], (*flats)[0]},
 		},
 		{
 			name:          "valid 3",
-			groupId:       groups[1].Id,
-			flatId:        flats[1].Id,
-			expectedFlats: []model.Flat{flats[1]},
+			groupId:       (*groups)[1].Id,
+			flatId:        (*flats)[1].Id,
+			expectedFlats: []model.Flat{(*flats)[1]},
 		},
 		{
 			name:          "group not found",
 			groupId:       111,
-			flatId:        flats[1].Id,
+			flatId:        (*flats)[1].Id,
 			expectedError: &e.KeyNotFound{Field: "groupId"},
 		},
 	}
@@ -88,6 +93,98 @@ func testGetGroupFlats(t *testing.T, flatRepo FlatRepository, groupRepo GroupRep
 					actualFlats, err := flatRepo.GetFlatsByGroupId(test.groupId, userId)
 					require.NoError(t, err)
 					assert.ElementsMatch(t, test.expectedFlats, actualFlats)
+				} else {
+					assert.EqualError(t, err, test.expectedError.Error())
+				}
+			})
+		}
+	})
+}
+
+func testCopyItemsFromFlatGroup(t *testing.T, repo ItemRepository,
+	flats *[]model.Flat, groups *[]model.Group, userId uint64) {
+	items := []model.Item{
+		{
+			Title:  "e",
+			UserId: userId,
+		},
+		{
+			Title:  "f",
+			UserId: userId,
+		},
+	}
+
+	tests := []struct {
+		name          string
+		groupId       uint64
+		flatId        uint64
+		flatIdCopy    uint64
+		items         []model.Item
+		expectedError error
+	}{
+		{
+			name:       "valid",
+			groupId:    (*groups)[0].Id,
+			flatId:     (*flats)[1].Id,
+			flatIdCopy: (*flats)[0].Id,
+			items:      items,
+		},
+		{
+			name:          "user has no access to flat flatId",
+			groupId:       (*groups)[1].Id,
+			flatId:        111,
+			flatIdCopy:    1,
+			expectedError: &e.NoAccess{Field: "userId,flatId"},
+		},
+		{
+			name:          "user has no access to flat flatIdCopy",
+			groupId:       (*groups)[1].Id,
+			flatId:        1,
+			flatIdCopy:    111,
+			expectedError: &e.NoAccess{Field: "userId,flatId"},
+		},
+		{
+			name:          "group not found",
+			groupId:       111,
+			flatId:        (*flats)[0].Id,
+			flatIdCopy:    (*flats)[1].Id,
+			expectedError: &e.KeyNotFound{Field: "groupId"},
+		},
+	}
+
+	t.Run("CopyItemsFromFlatGroup", func(t *testing.T) {
+		for _, test := range tests {
+			t.Run(test.name, func(t *testing.T) {
+				if test.items != nil {
+					for i := range test.items {
+						err := repo.CreateItem(&test.items[i])
+						require.NoError(t, err)
+
+						err = repo.AddItemToGroup(test.flatIdCopy, test.groupId, test.items[i].Id, userId)
+						require.NoError(t, err)
+					}
+				}
+
+				err := repo.CopyItemsFromFlatGroup(test.groupId, test.flatId, test.flatIdCopy, userId)
+
+				if test.expectedError == nil {
+					assert.Nil(t, err)
+
+					groupItemsExpected, err := repo.GetFlatItems(test.flatIdCopy, userId)
+					require.NoError(t, err)
+
+					groupItemsActual, err := repo.GetFlatItems(test.flatId, userId)
+					require.NoError(t, err)
+
+					groupItemsExpected = lo.Filter(groupItemsExpected, func(groupItem model.GroupItems, _ int) bool {
+						return groupItem.GroupId == test.groupId
+					})
+
+					groupItemsActual = lo.Filter(groupItemsActual, func(groupItem model.GroupItems, _ int) bool {
+						return groupItem.GroupId == test.groupId
+					})
+
+					assert.ElementsMatch(t, groupItemsExpected, groupItemsActual)
 				} else {
 					assert.EqualError(t, err, test.expectedError.Error())
 				}
